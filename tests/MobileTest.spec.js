d4e307b6e805590fbf1fa23bfb3aef347258c4ca
lloydtagami-cmd <lloyd.tagami@gmail.com>
2026-03-19 14:07:11 +0800

    mobile chu chu

--- tests/MobileTest.spec.js
+++ tests/MobileTest.spec.js
@@ -1,10 +1,21 @@
-const { test, devices } = require('@playwright/test');
+const { test, devices, expect } = require('@playwright/test');
+const loginPage = require('../pages/loginpage')
 const iPhone = devices['iPhone 13'];
 
 test.use({ ...iPhone }); // Applies emulation to all tests in this file
 
 test('my mobile emulation test', async ({ page }) => {
-  await page.goto('https://playwright.dev/docs/emulation');
-  // Your test steps here
+   await page.goto('https://freelance-learn-automation.vercel.app/login');
+  
+   await page.fill('input#email1', 'admin@email.com');
+   await page.fill('input#password1', 'admin@123');
+   await page.click("//button[text()='Sign in']");
+   await expect(page.locator('.welcomeMessage')).toContainText('Welcome');
+   await page.waitForTimeout(5000); // Wait for 5 seconds to allow the page to load after login
+   
+   //await page.locator.fill('input[name="email"]', 'admin@email.com');
+  //  await loginPage.Login('admin@email.com', 'admin@123');
+    //await expect(page.locator('.welcomeMessage')).toContainText('Welcome');
+  
   await page.screenshot({ path: 'mobile1.png' });
 });
