0d46a53c2cc2e7524c2f1dcb46eebcf18bb21913
lloydtagami-cmd <lloyd.tagami@gmail.com>
2026-04-01 02:09:53 +0000

    web vs code sample

---
+++ tests/Smoke_testing/testing.spec.js
@@ -0,0 +1,18 @@
+const {test,expect} = require('@playwright/test')
+
+
+test.("My First Test",async function({page}) {
+    expect(12).toBe(12)
+})
+
+test.skip("My Second Test",async function({page}) {
+    expect(100).toBe(111)
+})
+
+test.skip("My Third Test",async function({page}) {
+    expect(2.0).toBe(2.0)
+})
+
+test.skip("My Fourth Test",async function({page}) {
+    expect("Lloyd Tagami").toContain("Lloyd")
+})
