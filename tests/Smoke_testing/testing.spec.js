2b6cf1b7d000607d4bd3849b0d8dc94fc70e2376
lloydtagami-cmd <lloyd.tagami@gmail.com>
2026-04-01 10:13:34 +0800

    test web checkin

--- tests/Smoke_testing/testing.spec.js
+++ tests/Smoke_testing/testing.spec.js
@@ -1,7 +1,23 @@
-const {test,expect} = require('@playwright/test')
+import { test, expect } from '@playwright/test'
+
+test("My First Test",async function({page}) {
+    expect(12).toBe(12)
+})
+
+test.skip("My Second Test",async function({page}) {
+    expect(100).toBe(111)
+})
+
+test.skip("My Third Test",async function({page}) {
+    expect(2.0).toBe(2.0)
+})
+
+test.skip("My Fourth Test",async function({page}) {
+    expect("Lloyd Tagami").toContain("Lloyd")
+})
 
 
-test.("My First Test",async function({page}) {
+test("My First Test",async function({page}) {
     expect(12).toBe(12)
 })
 
